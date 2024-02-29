63a67fcc8b68e8ebc0afcf41854f4c0954754cd7
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2024-02-28 22:08:44 -0500

    Update flex-wrap property in container div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           </h2>
         </div>
 
-        <div className="mt-20 mx-auto container flex flex-wrap justify-center gap-[30px] md:justify-between">
+        <div className="mt-20 mx-auto container flex flex-wrap lg:flex-nowrap justify-center gap-[30px] md:justify-between">
           <CardSection title="Denim mens jacket" image="https://images.unsplash.com/photo-1491553895911-0055eca6402d?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
           <CardSection title="Winter Clothes" image="https://images.unsplash.com/photo-1608571423902-eed4a5ad8108?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
           <CardSection title="Man Winter Jecket" image="https://images.unsplash.com/photo-1505740106531-4243f3831c78?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
